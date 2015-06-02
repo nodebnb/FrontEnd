8475a2df82a31ee2889a93e4e4dc3c300f574584
Linghua Jin <ljin@walmartlabs.com>
2015-06-02 00:35:37 -0500

    updated search so it will work with filters

--- components/Search.js
+++ components/Search.js
@@ -1,18 +1,25 @@
+'use strict';
+/**
+ * Search and Filter will both access the route
+ * route will update the search store
+ */
 import React from 'react';
 import cx from 'classnames';
-import { navigateAction, RouteStore } from 'fluxible-router';
+import { navigateAction, RouteStore, handleRoute} from 'fluxible-router';
 import connectToStores from 'fluxible/addons/connectToStores';
+
 // import SearchStore from '../stores/SearchStore';
 
-import mui from 'material-ui';
+
+import UrlUtil from '../utils/UrlUtil';
 
 /**
  * Search updates Route by executing navigate action
  */
 
-
-let ThemeManager = require('material-ui/lib/styles/theme-manager')();
-let TextField = mui.TextField;
+// import mui from 'material-ui';
+// let ThemeManager = require('material-ui/lib/styles/theme-manager')();
+// let TextField = mui.TextField;
 
 const ENTER_KEY_CODE = 13;
 
@@ -24,14 +31,15 @@ class Search extends React.Component {
         };
     }
     // Important! for material UI
-    getChildContext() { 
-        return {
-          muiTheme: ThemeManager.getCurrentTheme()
-        };
-    }
+    // getChildContext() {
+    //     return {
+    //       muiTheme: ThemeManager.getCurrentTheme()
+    //     };
+    // }
 
 
     componentDidMount() {
+        //do not delete
         // this.context.executeAction(loadIndex);
         // // if on search page with query, then set state
         // const query = this.props.currentRoute.get('query').get('q');
@@ -43,6 +51,8 @@ class Search extends React.Component {
     }
 
     componentDidUpdate() {
+        //do not delete
+
         // const el = React.findDOMNode(this.refs.q);
         // // ensure input is focused
         // if (this.state.visible) {
@@ -55,27 +65,33 @@ class Search extends React.Component {
         // }
     }
 
-    _getPath() {
-        return this.context.getStore(RouteStore).makePath('search');
-    }
-
+    //TODO: find a more elegant way for generating the new url
     _onKeyDown(event) {
-        console.log(">< enter key down")
         if (event.keyCode === ENTER_KEY_CODE) {
             event.preventDefault();
             event.stopPropagation();
-            console.log("><event.target.value", event.target.value)
+            let key = 'q';
+            let value = event.target.value;
+                //detect if current route is search or not
+            let newUrl = this.props.currentNavigate.url;
+            //if not on search page, redirect to search page
+            if (newUrl.indexOf('search') < 0) {
+                newUrl = this.context.getStore(RouteStore).makePath('search');
+            }
+            const query = this.props.currentRoute.get('query').get(key);
+            if (query) {
+                newUrl = UrlUtil.updateQuery(newUrl, key, value);
+            } else {
+                newUrl = UrlUtil.addQuery(newUrl, key, value);
+            }
 
             this.context.executeAction(navigateAction, {
                 method: 'GET',
-                url: this._getPath() + '?q=' + event.target.value
+                url: newUrl
             });
         }
     }
-
     render() {
-        console.log(">< render")
-
         return (
                       <input
                         className="mainSearchBox"
@@ -95,9 +111,9 @@ Search.propTypes = {
     currentRoute: React.PropTypes.object
 };
  // Important! for material UI
-Search.childContextTypes = {
-  muiTheme: React.PropTypes.object
-};
+// Search.childContextTypes = {
+//   muiTheme: React.PropTypes.object
+// };
 
 // Search = connectToStores(Search, [ SearchStore ], function (stores, props) {
 //     return {
@@ -105,5 +121,6 @@ Search.childContextTypes = {
 //     };
 // });
 
+Search = handleRoute(Search);
 
 export default Search;
